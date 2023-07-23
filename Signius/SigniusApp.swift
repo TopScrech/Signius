cef0432cfa7f26586414bc5aa2892a2b3c2181dd
Sergei Saliukov <sergei_saliukov@icloud.com>
2023-07-23 23:01:12 +0200

    Initial Commit

---
+++ Signius/SigniusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SigniusApp.swift
+//  Signius
+//
+//  Created by Sergei Saliukov on 23/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SigniusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
