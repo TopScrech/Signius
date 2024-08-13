c32dde251ada9d2b32b922537ee6529eeaae5fbf
TopScrech <sergei_saliukov@icloud.com>
2024-08-13 06:31:04 +0200

    Initial Commit

---
+++ Signius/SigniusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SigniusApp.swift
+//  Signius
+//
+//  Created by Sergei Saliukov on 13/8/24.
+//
+
+import SwiftUI
+
+@main
+struct SigniusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
